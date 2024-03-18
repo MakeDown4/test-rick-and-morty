d711827c9dc92185dee3cab9c046e2db2bfdee92
Vinicius Pires <vinicius.pires@bradev.tech>
2024-03-18 13:41:11 -0300

    correcao_rotas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\Api\CharacterController;
 use App\Http\Controllers\Api\LocationController;
 use App\Http\Controllers\Api\EpisodeController;
 
-Route::get('/', [MainController::class, 'index']);
-
-Route::get('/characters', [CharacterController::class, 'index']);
-Route::get('/characters/{id}', [CharacterController::class, 'show']);
-
-Route::get('/locations', [LocationController::class, 'index']);
-Route::get('/locations/{id}', [LocationController::class, 'show']);
-
-Route::get('/episodes', [EpisodeController::class, 'index']);
-Route::get('/episodes/{id}', [EpisodeController::class, 'show']);
+Route::get('/', function () {
+    return Inertia::render('Main');
+})->name('main');
